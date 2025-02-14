5f74315ec8250c6aa167c9826a0e458db618fcfd
MariFZ <maritzafigueroazuleta@gmail.com>
2025-02-14 19:00:07 +0100

    esercizio 3.3

---
+++ Lezione_02 Tipi di dati_operatori_collection/Esercizio 3.3.py
@@ -0,0 +1,13 @@
+# 3-3. Your Own List
+
+#  Think of your favorite mode of transportation, 
+# such as a motorcycle or a car, and make a list that stores several examples. 
+# Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+mezzi: list=["bicicleta", "macchina", "treno"] 
+
+print("Vorrei avere una " + mezzi[0] + " e andare per il mondo su di essa")
+
+print("Vorrei avere una " + mezzi[1] + " per non dover camminare tutto il tempo")
+
+print("Mi piace andare in " + mezzi[2] + " per conoscere tanti posti nuovi vedendo nuovi paessagi")
